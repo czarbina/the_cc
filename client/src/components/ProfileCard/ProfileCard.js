df4a959e524221057ed987be3d667689d168f7c4
Sabrina Kesler <sabrina@getvictorious.com>
2018-01-23 13:05:15 -0800

    background image

--- client/src/components/ProfileCard/ProfileCard.js
+++ client/src/components/ProfileCard/ProfileCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Card, Col, CardTitle} from 'react-materialize';
+import { Row, Card, Col, CardTitle, Carousel} from 'react-materialize';
 
 const ProfileCard = props => {
 	console.log(props);
@@ -7,12 +7,17 @@ return (
 <div className="container" style={{width:"80%"}}>
 	<Row>
 		<Col s={4}>
-			<Row>
-				Things
-			</Row>
-			<Card className='extra-large'
+			
+			<Card className='extra-large z-depth-5'
+				style= {{
+    				position: "fixed",
+    				top: "80",
+    				left: "40",
+    				width: "300px"
+    				}}
+
 				header={<CardTitle image={props.image}
-				style={{height:"40%"}}>{props.username}
+				>{props.username}
 				</CardTitle>}
 				>
 				<p>Name: {props.name}</p>
