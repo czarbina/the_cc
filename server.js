1e0ba544a94002c25d5d2ce3f57b00b49be2758b
Sabrina Kesler <sabrina@getvictorious.com>
2018-01-24 10:54:54 -0800

    RAAHHH

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(routes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
