7c301a9ba34be2dc182be6c5ea0d9e83256e5d39
Erin Hagerty <erinhagerty@Erins-MacBook-Pro.local>
2018-01-24 11:17:57 -0800

    create-form

--- client/src/components/create-profile/create-profile.js
+++ client/src/components/create-profile/create-profile.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
 import Jumbotron from "../../components/Jumbotron";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 import API from "../../Utils/API";
+import "./create-profile.css";
+import $ from 'jquery';
 
 class createProfile extends Component {
 
@@ -48,65 +47,128 @@ class createProfile extends Component {
   render() {
     return (
       <div className="container">
+          <Jumbotron className="jumbo">
+            <h1>Create a Profile</h1>
+          </Jumbotron>
 
-            <Jumbotron>
-              <h1>Create a Profile</h1>
-            </Jumbotron>
+          <div className="row">
+              <form className="col s12">
+                <div className="row">
+                  <div className="input-field col s6">
+                    <i className="material-icons prefix">child_care</i>
+                    <input
+                        value={this.state.artistName}
+                        onChange={this.handleInputChange}
+                        name="artistName"
+                     id="icon_prefix" type="text" className="validate"/>
+                    <label for="icon_prefix">Artist Name</label>
+                  </div>
+                  <div className="input-field col s6">
+                    <i className="material-icons prefix">account_circle</i>
+                    <input
+                        value={this.state.coverphoto}
+                        onChange={this.handleInputChange}
+                        name="coverphoto"
+                    id="icon_account" type="tel" className="validate"/>
+                    <label for="icon_account">Image Link</label>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+            <div class="row">
+              <form class="col s12">
+                <div class="row">
+                  <div class="input-field col s12">
+                  <i className="material-icons prefix">comment</i>
+                    <textarea className="textarea"
+                        value={this.state.bio}
+                        onChange={this.handleInputChange}
+                        name="bio"
+                        placeholder="Bio"
+                    id="textarea1" class="materialize-textarea"></textarea>
+                    <label for="textarea1"></label>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+            <div className="row">
+                <form className="col s12">
+                  <div className="row">
+                    <div className="input-field col s6">
+                      <i className="material-icons prefix">fingerprint</i>
+                      <input
+                          value={this.state.tags}
+                          onChange={this.handleInputChange}
+                          name="tags"
+                       id="icon_prefix" type="text" className="validate"/>
+                      <label for="icon_prefix">Tags</label>
+                    </div>
+                  </div>
+                </form>
+              </div>
+
+              <div className="row">
+                  <form className="col s12">
+                    <div className="row">
+                      <div className="input-field col s4">
+                        <i className="material-icons prefix">cloud_circle</i>
+                        <input
+                            value={this.state.links}
+                            onChange={this.handleInputChange}
+                            name="links"
+                         id="icon_prefix" type="text" className="validate"/>
+                        <label for="icon_prefix">Sound Cloud Link</label>
+                      </div>
+                      <div className="input-field col s4">
+                        <i className="material-icons prefix">cloud_circle</i>
+                        <input
+                            value={this.state.links2}
+                            onChange={this.handleInputChange}
+                            name="links2"
+                        id="icon_account" type="tel" className="validate"/>
+                        <label for="icon_account">Sound Cloud Link</label>
+                      </div>
+                      <div className="input-field col s4">
+                        <i className="material-icons prefix">cloud_circle</i>
+                        <input
+                            value={this.state.links3}
+                            onChange={this.handleInputChange}
+                            name="links3"
+                         id="icon_prefix" type="text" className="validate"/>
+                        <label for="icon_prefix">Sound Cloud Link</label>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+
+                <div className="row">
+                    <form className="col s12">
+                      <div className="row">
+                        <div className="input-field col s6">
+                          <i className="material-icons prefix">child_care</i>
+                          <input
+                              value={this.state.facebook}
+                              onChange={this.handleInputChange}
+                              name="facebook"
+                           id="icon_prefix" type="text" className="validate"/>
+                          <label for="icon_prefix">Facebook Link</label>
+                        </div>
+                        <div className="input-field col s6">
+                          <i className="material-icons prefix">account_circle</i>
+                          <input
+                              value={this.state.soundcloud}
+                              onChange={this.handleInputChange}
+                              name="soundcloud"
+                          id="icon_account" type="tel" className="validate"/>
+                          <label for="icon_account">Sound Cloud Page Link</label>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+
             <form>
-              <Input
-                value={this.state.artistName}
-                onChange={this.handleInputChange}
-                name="artistName"
-                placeholder="Artist Name"
-              />
-              <Input
-                value={this.state.tags}
-                onChange={this.handleInputChange}
-                name="tags"
-                placeholder="Tags"
-              />
-              <TextArea
-                value={this.state.bio}
-                onChange={this.handleInputChange}
-                name="bio"
-                placeholder="Bio"
-              />
-              <Input
-                value={this.state.links}
-                onChange={this.handleInputChange}
-                name="links"
-                placeholder="Link (Soundcloud Embed src)"
-              />
-              <Input
-                value={this.state.links2}
-                onChange={this.handleInputChange}
-                name="links2"
-                placeholder="Link (Soundcloud Embed src)"
-              />
-              <Input
-                value={this.state.links3}
-                onChange={this.handleInputChange}
-                name="links3"
-                placeholder="Link (Soundcloud Embed src)"
-              />
-              <Input
-                value={this.state.coverphoto}
-                onChange={this.handleInputChange}
-                name="coverphoto"
-                placeholder="Image Link"
-              />
-              <Input
-                value={this.state.soundcloud}
-                onChange={this.handleInputChange}
-                name="soundcloud"
-                placeholder="Link to Soundcloud"
-              />
-              <Input
-                value={this.state.facebook}
-                onChange={this.handleInputChange}
-                name="facebook"
-                placeholder="Link to Facebook"
-              />
               <FormBtn
                 disabled={!(this.state.artistName)}
                 onClick={this.handleFormSubmit}
@@ -114,7 +176,6 @@ class createProfile extends Component {
                 Submit Profile
               </FormBtn>
             </form>
-
       </div>
     );
   }
