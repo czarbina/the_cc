15064b2cac77914fdca9a6596effddc8ec9b1ca0
Sabrina Kesler <sabrina@getvictorious.com>
2018-01-24 10:51:10 -0800

    RAAHHH

---
+++ client/build/js/app.js
@@ -0,0 +1,7 @@
+  import { Button, Card, Row, Col } from 'react-materialize';
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+          
