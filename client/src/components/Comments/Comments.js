8cbc27ce4430ec6df9a48ef33fe85c9e331e1619
Sabrina Kesler <sabrina@getvictorious.com>
2018-01-25 15:16:22 -0800

    build prod

---
+++ client/src/components/Comments/Comments.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Collapsible, CollapsibleItem } from 'react-materialize';
+import "./Comments.css";
+
+const Comments = props => {
+
+return (
+	<div className="container" style={{width:"65%", marginLeft:"410px"}}>
+	<div id="comments">
+	<Collapsible>
+	<CollapsibleItem header='from Sabrina' icon='favorite_border'>
+		Really love your work, man! Much love!
+	</CollapsibleItem>
+	<CollapsibleItem header='from Tyler' icon='favorite_border'>
+		Siiiiiick - keep it coming!
+	</CollapsibleItem>
+	<CollapsibleItem header='from Ned' icon='favorite_border'>
+		I'm looking for a DJ for my wedding, would love to discuss availability with you.
+	</CollapsibleItem>
+	<CollapsibleItem header='from Erin' icon='favorite_border'>
+		BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! BEER! 
+	</CollapsibleItem>
+	<CollapsibleItem header='from Clark' icon='favorite_border'>
+		This is amazing work. Truly, I am inspired by all of you.
+	</CollapsibleItem>
+	<CollapsibleItem header='from Elton' icon='favorite_border'>
+		BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!BEEPBEEPBOOPBOOPBEEPBEEPBOOPBOOPBEER! BEER! BEER! BEER!
+	</CollapsibleItem>
+	</Collapsible>
+	</div>
+	</div>
+)};
+
+export default Comments;
