da3f74e865520a0c72bd580ea8c8df6ba9b6fb34
Erin Hagerty <erinhagerty@Erins-MBP.attlocal.net>
2018-01-25 22:46:26 -0800

    bg

--- client/src/components/bio/bio.js
+++ client/src/components/bio/bio.js
@@ -5,8 +5,8 @@ import "./bio.css";
 const Bio = props => {
 return (
 	<div className="container">
-				<h5 className={props.bio !== null && props.bio !== "" ? 'visible container grey-text text-darken-2' : 'invisible'}>About Me</h5>
-				<p className="padding grey-text text-darken-2">{props.bio}</p>
+				<h5 className={props.bio !== null && props.bio !== "" ? 'visible container grey-text text-darken-2 cssfont' : 'invisible'}>About Me</h5>
+				<p className="cssfont padding grey-text text-darken-2">{props.bio}</p>
 	</div>)};
 
 export default Bio;
